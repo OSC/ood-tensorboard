410870d16c54d750dc5417851e5cfec4145271e0
Samir <mansour.69@osu.edu>
2019-08-14 11:11:09 -0400

    test

--- app.py
+++ app.py
@@ -1,28 +1,27 @@
-from threading import Lock
-from werkzeug.wsgi import pop_path_info, peek_path_info
+from werkzeug.wsgi import DispatcherMiddleware
 from fallback import MyApp
+from tensorboard.backend import application as backendWSGI
+from tensorboard import default
+import tensorflow as tf
+import os
 
-class PathDispatcher(object):
 
-    def __init__(self, default_app, create_app):
-        self.default_app = default_app
-        self.create_app = create_app
-        self.lock = Lock()
-        self.instances = {"/": MyApp}
+def make_app():
+    
+    return backendWSGI.standard_tensorboard_wsgi(
+        assets_zip_provider=default.get_assets_zip_provider(),
+        db_uri="",
+        logdir="/users/PZS0715/smansour/tensorboardTestbench/logs",
+        purge_orphaned_data="",
+        reload_interval=5,
+        plugins=default.get_plugins(),
+        path_prefix=prefix,
+        window_title="",
+        max_reload_threads=1
+        )
 
-    def get_application(self, prefix):
-        with self.lock:
-            app = self.instances.get(prefix)
-            if app is None:
-                app = self.create_app(prefix)
-                if app is not None:
-                    self.instances[prefix] = app
-            return app
 
-    def __call__(self, environ, start_response):
-        app = self.get_application(peek_path_info(environ))
-        if app is not None:
-            pop_path_info(environ)
-        else:
-            app = self.default_app
-        return app(environ, start_response)
+
+application = DispatcherMiddleware(MyApp, {
+    '/load': make_app     
+})
--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,29 +1,9 @@
-from app import PathDispatcher
-from fallback import MyApp
-from tensorboard.backend import application as backendWSGI
-from tensorboard import default
-import tensorflow as tf
-import os
+from app import application as app
 
 
-default_app = MyApp
-
-def make_app(prefix):
-	if prefix is not "/":
-		return backendWSGI.standard_tensorboard_wsgi(
-			assets_zip_provider=default.get_assets_zip_provider(),
-			db_uri="",
-			logdir="/users/PZS0715/smansour/tensorboardTestbench/logs",
-			purge_orphaned_data="",
-			reload_interval=5,
-			plugins=default.get_plugins(),
-			path_prefix=prefix,
-			window_title="",
-			max_reload_threads=1
-			)
 
 
-application = PathDispatcher(default_app, make_app)
+application = app
 
 if __name__ == '__main__':
     application.run()
