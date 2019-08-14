6136ad1761d81eb8d7d8cb63d4976c57212b8de8
Samir <mansour.69@osu.edu>
2019-08-14 09:32:17 -0400

    PathDispatcher tweak

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ class PathDispatcher(object):
         self.default_app = default_app
         self.create_app = create_app
         self.lock = Lock()
-        self.instances = {"/": default_app}
+        self.instances = {}
 
     def get_application(self, prefix):
         with self.lock:
@@ -18,10 +18,10 @@ class PathDispatcher(object):
                     self.instances[prefix] = app
             return app
 
-    def __call__(self, environ):
+    def __call__(self, environ, start_response):
         app = self.get_application(peek_path_info(environ))
-        if app is None:
-            app = self.create_app
+        if app is not None:
+            pop_path_info(environ)
         else:
             app = self.default_app
-        return app(environ)
+        return app(environ, start_response)
