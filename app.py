d18f49428c8773cc72d9bcde91d6a7c009ddce8e
Eric Franz <efranz@osc.edu>
2019-08-19 21:09:10 -0400

    get dummy app working with path dispatcher

--- app.py
+++ app.py
@@ -5,14 +5,17 @@ from tensorboard import default
 import tensorflow as tf
 import os
 from werkzeug.wsgi import get_query_string
-from tensorboard.backend import application
 from flask import request
+from threading import Lock
+from werkzeug.wsgi import pop_path_info, peek_path_info
+from flask import Flask
+import pprint
 
 
 #print(get_query_string(environ))
 def create_tb_app(log):
 
-  return application.standard_tensorboard_wsgi(
+  return backendWSGI.standard_tensorboard_wsgi(
       assets_zip_provider=default.get_assets_zip_provider(),
       db_uri='',
       logdir=os.path.expanduser(log),
@@ -25,7 +28,42 @@ def create_tb_app(log):
       flags='')
 
 
+def create_dummy_app(details):
+  app = Flask(__name__, instance_relative_config=True)
+
+  @app.route('/')
+  def index():
+      return "<h2>" + ("/") + "</h2><pre>" + details + "</pre>"
+
+  @app.route('/<path:subpath>')
+  def index_with_subpath(subpath):
+      return "<h2>" + (subpath or "None") + "</h2><pre>" + details + "</pre>"
+
+  return app
+
+# path dispatcher will accept a reference to an instances map
+# but not be responsible for creating new apps
+
+class PathDispatcher(object):
+    def __init__(self, default_app, instances):
+        self.default_app = default_app
+        self.instances = instances
+
+    def __call__(self, environ, start_response):
+        # my_app = pprint.pformat(self.instances.get(peek_path_info(environ)))
+        my_app = create_dummy_app(pprint.pformat(self.instances) +
+                                  "\n\npeek_path_info: " +
+                                  peek_path_info(environ) + "\n\napp found:" + pprint.pformat(self.instances.get(peek_path_info(environ))))
+
+        if my_app is not None:
+            pop_path_info(environ)
+        else:
+            my_app = self.default_app
+        return my_app(environ, start_response)
+
 application = DispatcherMiddleware(MyApp, {
-    '/one': create_tb_app("/users/PZS0715/smansour/TensorboardTestbench/logs/"),
-    '/two': create_tb_app("/users/PZS0562/efranz/tmp/TensorboardTestbench/logs/")
+    '/instances': PathDispatcher(MyApp, {
+        'one': create_tb_app("/users/PZS0715/smansour/TensorboardTestbench/logs/"),
+        'two': create_tb_app("/users/PZS0562/efranz/tmp/TensorboardTestbench/logs/")
+    })
 })
